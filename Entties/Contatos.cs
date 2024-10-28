f2fb11e5541e0fc2a515dba71297c4d2f8c64024
fedlferreira <ffylepe@hotmail.com>
2024-10-28 11:39:12 -0300

    Add project files.

---
+++ Entties/Contatos.cs
@@ -0,0 +1,10 @@
+﻿namespace ModuloAPI.Entities
+{
+    public class Contato
+    {
+        public int  Id{ get; set; }
+        public string Name{ get; set; }
+        public string Telefone{ get; set; }
+        public bool Ativo { get; set; }
+    }
+}
